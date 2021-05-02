70178867e91cd87fdf3a91ad831467fdf5d90a05
Ruslan-Borisov <78921891+Ruslan-Borisov@users.noreply.github.com>
2021-05-02 15:55:37 +0300

    add

---
+++ client/src/main/java/Controller.java
@@ -0,0 +1,22 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    TextField msgField;
+
+
+    public void sendMsg(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
