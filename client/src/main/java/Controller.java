6bdbe10fc466038db26334108a06852cd6d2b8a4
Ruslan-Borisov <78921891+Ruslan-Borisov@users.noreply.github.com>
2021-05-03 14:59:15 +0300

    add

--- client/src/main/java/Controller.java
+++ client/src/main/java/Controller.java
@@ -1,22 +1,2 @@
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-
-import java.awt.*;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class Controller implements Initializable {
-    @FXML
-    TextField msgField;
-
-
-    public void sendMsg(ActionEvent actionEvent) {
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
+public class Controller {
 }
--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -8,21 +8,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        System.out.println("Вошли в start");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        System.out.println("Вошли указали путь к sample.fxml");
         primaryStage.setTitle("Hello World");
-        System.out.println("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-        System.out.println("Размер экран");
         primaryStage.show();
-        System.out.println("Активировали окно");
-
     }
 
 
     public static void main(String[] args) {
-
         launch(args);
     }
 }
